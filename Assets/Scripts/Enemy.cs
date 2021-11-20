bb8473e0665fb81cb5bcc174d2d914d22d5a5450
Angus Anthony <31295232+m0nsoon-dev@users.noreply.github.com>
2021-11-20 11:03:17 -0400

    Enemy navigation based on sound and vision.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,27 +4,37 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private Transform player;
     [SerializeField] private float health = 10.0f;
     [SerializeField] private float visibleTime = 0.05f; //how long in total enemy will remain visible while no longer in field of vision
     [SerializeField] private Transform firePoint; //where the bullets spawn from
     [SerializeField] private GameObject bulletPrefab; //select bullet prefab
+    [SerializeField] private LayerMask layerMask;
 
-    private Rigidbody2D body;
     private SpriteRenderer spriteRenderer;
     private float timer; //how long left for enemy to reamin visible while no longer in field of vision
-    private bool shooting;
     private bool heardPlayer;
+    private Vector2 noiseLocation; //where the noise came from
+    private bool canSeePlayer;
     private float bulletForce;
+
+    private ContactFilter2D filter;
+    private float viewDistance = 10.0f; //CHANGE LATER DEPENDING ON WEAPON
+    private float fov = 60.0f; //CHANGE LATER DEPENDING ON WEAPON 
 
     void Start()
     {
-        shooting = false;
         heardPlayer = false;
+        canSeePlayer = false;
         bulletForce = 30.0f;
         spriteRenderer = GetComponent<SpriteRenderer>();
-        body = GetComponent<Rigidbody2D>();
-        StartCoroutine("shootRoutine");
+
+        //StartCoroutine("shootRoutine");
+
+        StartCoroutine("fovCheckRoutine");
+
+        filter = new ContactFilter2D();
+        filter.useLayerMask = true;
+        filter.layerMask = LayerMask.GetMask("Player");
     }
 
     void Update()
@@ -38,12 +48,6 @@ public class Enemy : MonoBehaviour
                 spriteRenderer.enabled = false; //make invisible
             }
         }
-
-        Vector3 direction = player.position - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        body.rotation = angle;
-
-
     }
 
     //makes this enemy visible while in player's field of view
@@ -64,6 +68,7 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    //TEMPORARY TEST SHOOTING METHOD
     private void shoot()
     {
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation) as GameObject;
@@ -77,6 +82,7 @@ public class Enemy : MonoBehaviour
         rb.AddForce(firePoint.right * bulletForce, ForceMode2D.Impulse);
     }
 
+    //TEMPORARY TEST SHOOTING ROUTINE
     IEnumerator shootRoutine()
     {
         for (; ; )
@@ -86,10 +92,90 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    //set where this enemy last heard the player
     public void setHeard(bool status, Vector2 position)
     {
         heardPlayer = status;
-        Debug.Log("I HEARD THE PLAYER AT" + position.ToString());
+        noiseLocation = position;
+        Debug.Log("I HEARD THE PLAYER AT" + noiseLocation.ToString());
     }
 
+    //get whether this enemy heard the player or not
+    public bool getHeard()
+    {
+        return heardPlayer;
+    }
+
+    //get the position at which this enemy heard the player
+    public Vector2 getNoiseLocation()
+    {
+        return noiseLocation;
+    }
+
+    //set whether this enemy can currently see the player
+    public void setCanSeePlayer(bool status, Vector2 position)
+    {
+        canSeePlayer = status;
+        Debug.Log("I SEE THE PLAYER AT" + position.ToString());
+    }
+
+    //get whether this enemy can currently see the player
+    public bool getCanSeePlayer()
+    {
+        return canSeePlayer;
+    }
+
+    //checks to see if the player is currently in this enemy's field of view.
+    private void fovCheck()
+    {
+        //create contact filter to only check for enemies
+        ContactFilter2D filter = new ContactFilter2D();
+        filter.useLayerMask = true;
+        filter.layerMask = LayerMask.GetMask("Player");
+
+        Collider2D[] results = new Collider2D[1];
+        int enemiesFound = Physics2D.OverlapCircle(transform.position, viewDistance, filter, results);
+
+        if (!(results[0] is null))
+        {
+            Transform target = results[0].transform;
+            Vector2 directionToTarget = (target.position - transform.position).normalized;
+            if (Vector2.Angle(transform.right, directionToTarget) < fov)
+            {
+                float distanceToTarget = Vector2.Distance(transform.position, target.position);
+
+                if (!Physics2D.Raycast(transform.position, directionToTarget, distanceToTarget, layerMask))
+                {
+                    canSeePlayer = true;
+                    Debug.Log("I CAN SEE THE PLAYER");
+                }
+                else
+                {
+                    canSeePlayer = false;
+                }
+            }
+            else
+            {
+                canSeePlayer = false;
+            }
+        }
+        else if (canSeePlayer)
+        {
+            canSeePlayer = false;
+        }
+    }
+
+    //routine to run the fov check periodically
+    private IEnumerator fovCheckRoutine()
+    {
+        float delay = 0.2f;
+        WaitForSeconds wait = new WaitForSeconds(delay);
+
+        while (true)
+        {
+            yield return wait;
+            fovCheck();
+        }
+    }
 }
+
--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,11 +5,17 @@ using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
+    [SerializeField] private Transform player;
     [SerializeField] protected Transform target;
-    NavMeshAgent agent;
+    [SerializeField] protected NavMeshAgent agent;
+
+    private Rigidbody2D body;
+    private Enemy e;
 
     void Start()
     {
+        e = GetComponent<Enemy>();
+        body = GetComponent<Rigidbody2D>();
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
@@ -17,6 +23,23 @@ public class EnemyMovement : MonoBehaviour
 
     void Update()
     {
-        agent.SetDestination(target.position);
+        //go investigate the noise
+        if (e.getHeard()) 
+        {
+            //agent.SetDestination(target.position);
+            agent.SetDestination(e.getNoiseLocation());
+            Vector3 direction = player.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            body.rotation = angle;
+        }
+
+        //look at the player
+        else if (e.getCanSeePlayer())
+        {
+            Vector3 direction = player.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            body.rotation = angle;
+        }
+        
     }
 }
